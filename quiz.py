372cea126bbbfcfdebef63195511e1bf5a1ec80a
jefferson5252173 <jeffersonzambrano@colegioguanenta.edu.co>
2024-03-13 07:38:53 -0500

    se agrego python y diagrama del programa

--- quiz.py
+++ quiz.py
@@ -6,7 +6,18 @@ print("----- VERIFICAR TRIANGULO ----")
 print("------------------------------")
 
 # input
+a = float(input("Ingrese el valor de a : "))
+b = float(input("Ingrese el valor de b : "))
+c = float(input("Ingrese el valor de c : "))
 
 # processing
+if a + b > c:
+    print("los numeros si forman un triangulo")
+elif a + c > b:
+    print("los numeros si forman un triangulo")
+elif b + c > a:
+    print("los numeros si forman un triangulo")
+else:
+    print("los numeros no forman un triangulo")
 
 # output
